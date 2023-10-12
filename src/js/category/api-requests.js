2d096c65835b49e8c4f716c718ce92fa816be76a
DeadMakar <ssapientiaa@gmail.com>
2023-10-12 19:06:43 +0300

    viti-config

--- src/js/category/api-requests.js
+++ src/js/category/api-requests.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const BASEURL = 'https://books-backend.p.goit.global/books/'
-
+const BASEURL = 'https://books-backend.p.goit.global/books/';
 
 export async function getBookCategory() {
-    const URL = `${BASEURL}category-list`
-    const response = await axios(URL);
-    return response.data;
+  const URL = `${BASEURL}category-list`;
+  const response = await axios.get(URL);
+  return response.data;
 }
