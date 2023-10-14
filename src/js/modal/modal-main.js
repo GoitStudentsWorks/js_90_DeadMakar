70563f72ad16e138ff0ce23b1dadb7fe310389b5
DeadMakar <ssapientiaa@gmail.com>
2023-10-14 16:03:43 +0300

    add

--- src/js/modal/modal-main.js
+++ src/js/modal/modal-main.js
@@ -1,15 +1,28 @@
-import { getAndDisplayBookById } from './modal-bookmarkup';
+import { fetchBookById } from './modal-api';
+import { saveBookToStorage, getStoredBooks } from './modal-local-storage';
 
-const list = document.querySelector('.category-list');
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', async function (event) {
+    if (event.target.classList.contains('book-card')) {
+      const bookId = event.target.dataset.bookId;
 
-list.addEventListener('click', async event => {
-  if (event.target.classList.contains('category-item')) {
-    const bookId = event.target.dataset.id;
+      try {
+        const bookData = await fetchBookById(bookId);
+        await saveBookToStorage(bookId, bookData);
 
-    try {
-      await getAndDisplayBookById(bookId);
-    } catch (error) {
-      console.error('Error handling click event:', error);
+        const buyLinks = bookData.buy_links;
+        const amazonLink = buyLinks.find(link => link.name === 'Amazon');
+        const appleBooksLink = buyLinks.find(
+          link => link.name === 'Apple Books'
+        );
+
+        const storedBooks = await getStoredBooks();
+        console.log('Stored Books:', storedBooks);
+
+        // Ваш код для використання отриманих даних
+      } catch (error) {
+        console.error('Error:', error);
+      }
     }
-  }
+  });
 });
