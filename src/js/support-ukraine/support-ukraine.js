a870ca09be88763d31669670cfa210b5c2209ff2
TonichUA <132302438+TonichUA@users.noreply.github.com>
2023-10-14 18:44:15 +0300

    аа

--- src/js/support-ukraine/support-ukraine.js
+++ src/js/support-ukraine/support-ukraine.js
@@ -1,1 +1,21 @@
+// Імпортуємо бібліотеку Swiper
+import Swiper from 'swiper/bundle';
 
+// Ініціалізуємо Swiper
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical', // Прокручування вниз
+  slidesPerView: '9', // Кількість видимих слайдів
+  freeMode: true, // Вільний режим прокрутки
+});
+
+// Отримуємо номер слайда, на який потрібно прокрутити
+const targetSlideIndex = 9;
+
+// Отримуємо кнопку
+const scrollToSlideBtn = document.getElementById('scrollToSlideBtn');
+
+// Додаємо обробник події для кнопки
+scrollToSlideBtn.addEventListener('click', function () {
+  // Прокручуємо до вказаного слайда з плавністю
+  swiper.slideTo(targetSlideIndex, 1000, false);
+});
