5e095aff70852f7d14fb831911808966b4392e9a
ilona1009 <maliarenko.ilona@gmail.com>
2023-10-15 19:24:46 +0200

    без img
    без img  в модалці

--- src/js/modal/modal-bookmarkup.js
+++ src/js/modal/modal-bookmarkup.js
@@ -1,19 +1,26 @@
 import { fetchBookById } from './modal-api';
 
-const modalContainer = document.querySelector('.modal-information');
+const modalInformation = document.querySelector('.modal-information');
+const conteinerImg = document.querySelector('.conteinerImg');
 const amazonLinkRef = document.querySelector('.modal-amazon-link');
 const appleBookLinkRef = document.querySelector('.modal-applebook-link');
 
+export function generateBookMarkupImg(bookData) {
+  const testMarkupImg = `
+    <img class="modal-book-image" src="${bookData.book_image}" alt="${bookData.title}"/>
+  `;
+  conteinerImg.innerHTML = testMarkupImg;
+}
+
 export function generateBookMarkup(bookData) {
   const testMarkup = `
-    <img class="modal-book-image" src="${bookData.book_image}" alt="${bookData.title}" />
     <div class="modal-box">
     <h3 class="modal-book-title">${bookData.title}</h3>
     <p class="modal-author">Author: ${bookData.author}</p>
     <p class="modal-description">${bookData.description}</p>
     </div>
   `;
-  modalContainer.innerHTML = testMarkup;
+  modalInformation.innerHTML = testMarkup;
 }
 
 export function addLinkShop(bookData) {
