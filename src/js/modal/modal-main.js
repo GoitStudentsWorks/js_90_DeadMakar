40958706bb7f60486117dc4163fa441bb256bff5
DeadMakar <ssapientiaa@gmail.com>
2023-10-15 16:31:20 +0300

    open-close-modal

--- src/js/modal/modal-main.js
+++ src/js/modal/modal-main.js
@@ -9,9 +9,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
       try {
         const bookData = await fetchBookById(bookId);
+        openModal();
       } catch (error) {
         console.error('Error:', error);
       }
     }
   });
+
+  // Закриття модального вікна при кліку на хрестик
+  const closeIcon = document.querySelector('.modal-icon-x-close');
+  closeIcon.addEventListener('click', function () {
+    closeModal();
+  });
+
+  // Закриття модального вікна при натисканні ESC
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  });
+
+  // Закриття модального вікна при кліку за межами модального вікна
+  document.addEventListener('click', function (event) {
+    if (!event.target.closest('.modal-container')) {
+      closeModal();
+    }
+  });
 });
+
+function openModal() {
+  const modal = document.querySelector('.modal-section');
+  modal.classList.remove('visually-hidden');
+}
+
+function closeModal() {
+  const modal = document.querySelector('.modal-section');
+  modal.classList.add('visually-hidden');
+}
