2408ce119689cf7f6d3e56cea725e928d1550473
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-10-16 21:04:06 +0300

    first end

--- src/js/basket/basket-selectors.js
+++ src/js/basket/basket-selectors.js
@@ -2,5 +2,5 @@ export const selectors = {
   ShopUlEl: document.querySelector('.shopping-list-container'),
   ShopStubEl: document.querySelector('.shopping-list-container'),
   shopRemoveBtnEl: document.querySelector('.shopping-delete-btn'),
-  targetEl: document.querySelector('.js-guard'),
+  // targetEl: document.querySelector('.js-guard'),
 };
